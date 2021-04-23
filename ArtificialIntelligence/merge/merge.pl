8c663c1665b7e1d60799b13258f249ee97925205
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2021-04-23 11:43:24 +0530

    Finally done with this :)

---
+++ ArtificialIntelligence/merge/merge.pl
@@ -0,0 +1,13 @@
+go:-
+    write('Input first list : '),
+    read(L1),
+    write('Input second list : '),
+    read(L2),
+    merge(L1,L2,L3),
+    write('The merged list is '),
+    write(L3).
+    
+merge([],L2,L2).
+merge(L1,[],L1).
+merge([H1|T1],[H2|T2],[H1|T3]):- H1=<H2,merge(T1, [H2|T2], T3).
+merge([H1|T1],[H2|T2],[H2|T3]):- merge([H1|T1], T2, T3).
